f0663bda47953b1cb99f7953eee4bd8ff32a1f83
trvchung <travischungg@gmail.com>
2024-03-12 22:04:24 -0700

    changed styling for WareHouseListMenu to match mockup for tablet and desktop responsive design. added a submenu for tablet and desktop responsive design that is hidden during mobile view with display none.

---
+++ src/components/WareHouseList/WareHouseListSubMenu/WareHouseListSubMenu.jsx
@@ -0,0 +1,28 @@
+function WareHouseListSubMenu() {
+  return (
+    <div className="ware-house-list-sub-menu">
+      <div className="ware-house-list-sub-menu__subcontainer">
+        <h4 className="ware-house-list-sub-menu__title">WAREHOUSE</h4>
+        <button className="ware-house-list-sub-menu__icon">.</button>
+      </div>
+      <div className="ware-house-list-sub-menu__subcontainer">
+        <h4 className="ware-house-list-sub-menu__title">ADDRESS</h4>
+        <button className="ware-house-list-sub-menu__icon">.</button>
+      </div>
+      <div className="ware-house-list-sub-menu__subcontainer">
+        <h4 className="ware-house-list-sub-menu__title">CONTRACT NAME</h4>
+        <button className="ware-house-list-sub-menu__icon">.</button>
+      </div>
+      <div className="ware-house-list-sub-menu__subcontainer">
+        <h4 className="ware-house-list-sub-menu__title">
+          CONTRACT INFORMATION
+        </h4>
+        <button className="ware-house-list-sub-menu__icon">.</button>
+      </div>
+      <div className="ware-house-list-sub-menu__subcontainer">
+        <h4 className="ware-house-list-sub-menu__title">ACTION</h4>
+      </div>
+    </div>
+  );
+}
+export default WareHouseListSubMenu;
--- src/pages/WareHousesPage/WareHousesPage.jsx
+++ src/pages/WareHousesPage/WareHousesPage.jsx
@@ -1,9 +1,11 @@
 import WareHouseListMenu from "../../components/WareHouseList/WareHouseLIstMenu/WareHouseListMenu";
 import WareHouseListSelection from "../../components/WareHouseList/WareHouseListSelection/WareHouseListSelection";
+import WareHouseListSubMenu from "../../components/WareHouseList/WareHouseListSubMenu/WareHouseListSubMenu";
 function WareHousesPage() {
   return(
     <>
       <WareHouseListMenu />
+      <WareHouseListSubMenu/>
       <WareHouseListSelection/>
     </>
   );
