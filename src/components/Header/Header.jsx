9a37184afff8198427b3bc0c6700fee11f98f213
Toba0z <tobgabby@aol.com>
2024-03-12 18:25:25 -0700

    completed Header and footer styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,48 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom'; // Import NavLink from react-router-dom
-import flixLogo from '../../assets/Logo/InStock-Logo_1x.png';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import flixLogo from "../../assets/Logo/InStock-Logo_1x.png";
 
 const Header = () => {
-    return (
-      <header className="header">
-        <div className="header__title header__title--logo">
+  // State to manage active button
+  const [activeButton, setActiveButton] = useState("warehouse"); // Default to 'warehouse'
+
+  return (
+    <header className="header">
+      <div className="header__border">  
+      <div className="header__title header__title--logo">
+        <NavLink to="/">
+          <img src={flixLogo} alt="BrainFlix Logo" className="header__logo" />
+        </NavLink>
+      </div>
+      <div className="header__actions">
+        <div className="header__warehouse">
           <NavLink to="/">
-            <img src={flixLogo} alt="BrainFlix Logo" className="header__logo" />
+            <button
+              className={`header__button ${
+                activeButton === "warehouse" ? "header__button--active" : ""
+              }`}
+              onClick={() => setActiveButton("warehouse")}
+            >
+              Warehouse
+            </button>
           </NavLink>
         </div>
-        <div className="header__actions">
-          <div className="header__warehouse">
-            <NavLink to="/">
-              <button className="header__upload">Warehouse</button>
-            </NavLink>
-          </div>
-  
-          <div className="header__inventory">
-            <NavLink to="/">
-              <button className="header__upload">Inventory</button>
-            </NavLink>
-          </div>
+        <div className="header__inventory">
+          <NavLink to="/">
+            <button
+              className={`header__button ${
+                activeButton === "inventory" ? "header__button--active" : ""
+              }`}
+              onClick={() => setActiveButton("inventory")}
+            >
+              Inventory
+            </button>
+          </NavLink>
         </div>
-      </header>
-    );
-  };
-  
-  export default Header;
+      </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
