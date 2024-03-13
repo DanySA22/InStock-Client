fa98987ec6b8279ff09178821866341d65af5a99
trvchung <travischungg@gmail.com>
2024-03-12 23:09:11 -0700

    tried matching sub menu with selection to have title and content to be parallel. will need to find a different solution

--- src/components/WareHouseList/WareHouseListSelection/WareHouseListSelection.jsx
+++ src/components/WareHouseList/WareHouseListSelection/WareHouseListSelection.jsx
@@ -12,7 +12,7 @@ function WareHouseListSelection() {
           </div>
           <div className="ware-house-list-selection__subcontainer">
             <h4 className="ware-house-list-selection__title">ADDRESS</h4>
-            <p className="ware-house-list-selection__info">
+            <p className="ware-house-list-selection__info ware-house-list-selection__info--address">
             503 Broadway, New York, USA
             </p>
           </div>
@@ -20,12 +20,12 @@ function WareHouseListSelection() {
         <div className="ware-house-list-selection__container">
           <div className="ware-house-list-selection__subcontainer">
             <h4 className="ware-house-list-selection__title">CONTACT NAME</h4>
-            <p className="ware-house-list-selection__info">
+            <p className="ware-house-list-selection__info ware-house-list-selection__info--name">
             Parmin Aujla
             </p>
           </div>
           <div className="ware-house-list-selection__subcontainer">
-            <h4 className="ware-house-list-selection__title">CONTACT INFORMATION</h4>
+            <h4 className="ware-house-list-selection__title ware-house-list-selection__info--info">CONTACT INFORMATION</h4>
             <p className="ware-house-list-selection__info">
             +1 (629) 555-0129 paujla@instock.com
             </p>
