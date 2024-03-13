bf1db286795ff413e2246c964d7edb57362be408
trvchung <travischungg@gmail.com>
2024-03-13 13:16:23 -0700

    created delete warehouse functionality. created a delete warehouse component that can be reused.

--- src/components/WareHouseList/WareHouseListSelection/WareHouseListSelection.jsx
+++ src/components/WareHouseList/WareHouseListSelection/WareHouseListSelection.jsx
@@ -1,13 +1,25 @@
 import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import DeleteWareHousePopup from "../DeleteWareHouse/DeleteWareHouse";
 
 function WareHouseListSelection() {
+  //useState is initialized with false to indicate pop up is closed. 
+  const [isPopupOpen, setPopupOpen] = useState(false);
+  // toggle function toggeles the isPopupOpen State
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
+
   return (
     <>
       <div className="ware-house-list-selection">
         <div className="ware-house-list-selection__container">
           <div className="ware-house-list-selection__subcontainer">
             <h4 className="ware-house-list-selection__title">WAREHOUSE</h4>
-            <NavLink className="ware-house-list-selection__link" to="/wareHouseDetails">
+            <NavLink
+              className="ware-house-list-selection__link"
+              to="/wareHouseDetails"
+            >
               <button className="ware-house-list-selection__button">
                 Manhattan
               </button>
@@ -38,12 +50,45 @@ function WareHouseListSelection() {
           </div>
         </div>
         <div className="ware-house-list-selection__action">
-          <button className="ware-house-list-selection__delete">d</button>
-          <NavLink className="ware-house-list-selection__link" to="/editWareHouse">
-          <button className="ware-house-list-selection__edit">e</button>
+          <button
+            className="ware-house-list-selection__delete"
+            onClick={togglePopup}
+          >
+            d
+          </button>
+          <NavLink
+            className="ware-house-list-selection__link"
+            to="/editWareHouse"
+          >
+            <button className="ware-house-list-selection__edit">e</button>
           </NavLink>
         </div>
       </div>
+      <DeleteWareHousePopup isOpen={isPopupOpen} onClose={togglePopup}>
+        <div class="ware-house-list-selection__popup">
+          <h1 className="ware-house-list-selection__header">
+            Delete Washington warehouse?
+          </h1>
+          <p className="ware-house-list-selection__text">
+            {" "}
+            Please confirm that you'd like to delete the Washington warehouse
+            from the list of warehouses. you won't be able to undo this action
+          </p>
+          <div className="ware-house-list-selection__box">
+            <button
+              className="ware-house-list-selection__button-2 ware-house-list-selection__button-2--cancel"
+              onClick={togglePopup}
+            >
+              cancel
+            </button>
+            <form className="delete">
+            <button className="ware-house-list-selection__button-2 ware-house-list-selection__button-2--delete">
+              delete
+            </button>
+            </form>
+          </div>
+        </div>
+      </DeleteWareHousePopup>
     </>
   );
 }
