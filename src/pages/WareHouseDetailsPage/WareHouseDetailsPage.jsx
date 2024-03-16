752e9db317c3e5a4333bfdbedcd9216c48010c3d
DanySA22 <winache22@gmail.com>
2024-03-16 09:09:33 -0700

    Change after merge

--- src/pages/WareHouseDetailsPage/WareHouseDetailsPage.jsx
+++ src/pages/WareHouseDetailsPage/WareHouseDetailsPage.jsx
@@ -1,6 +1,22 @@
+import EditWareHouseHeader from "../../components/WareHouseList/EditWareHouseHeader/EditWareHouseHeader";
+
 function WareHouseDetailsPage(){
+  console
+  
   return (
-    <div>warehouse Details</div>
+    <>
+    <section className="edit-whsheader">
+      <NavLink to="/" className="edit-whsheader__arrow-back">
+        <img
+          src={arrowIcon}
+          alt="Go back"
+          className="edit-whsheader__buttonicon"
+        />
+      </NavLink>
+      <div className="edit-whsheader__title">Edit Warehouse</div>
+    </section>
+    <div>warehouse Details2</div>
+    </>
   )
 }
 export default WareHouseDetailsPage;
